c000b59ae43429e933231cd82ae4808f322dd47a
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2021-03-15 23:09:18 +0600

    Create Paparazzi Gennady.cpp
    Convex Hull

---
+++ Convex Hull/Paparazzi Gennady.cpp
@@ -0,0 +1,160 @@
+///   BISMILLAHIR RAHMANIR RAHEEM
+///   ALLAH IS WATCHING ME
+
+
+//█▀█─█──█──█▀█─█─█
+//█▄█─█──█──█▄█─█▄█
+//█─█─█▄─█▄─█─█─█─█
+
+///   Every soul shall taste death.
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+
+
+///  order_of_key return number of elements less than x.
+///  find_by_order return index.
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define MUHAMMAD        ios::sync_with_stdio(0);cin.tie(0);
+#define all(x)          (x).begin(), (x).end()
+#define AE              cout << fixed << setprecision(10);
+#define ld              double
+
+
+/// faster.
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+
+using ll = long long;
+const ll MOD = 1e9 + 7;
+
+
+typedef tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+
+//debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+
+/// Modular arithmetic
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+//***********************************************  The END **********************************************************************************************************************************
+
+const ll N = 2e5 + 5;
+const ll INF = 1e18;
+const ld EPS   = 1e-9;
+const ll limit = (1LL<<32) - 1;
+const int K = 1e3 + 1;
+
+/// Global...
+
+ll slope ( vector < pair < ll , ll > > v , ll x2 , ll y2 ){
+
+       ll len = v.size();
+
+       ll x = v[len-2].first;
+       ll y = v[len-2].second;
+
+       ll x1 = v[len-1].first;
+       ll y1 = v[len-1].second;
+
+       return (x1-x)*(y2-y) - (x2-x)*(y1-y);
+}
+
+void solve ( int tc ){
+
+   ll n;
+   cin >> n;
+
+   ll arr[n];
+   for ( int i = 0 ; i < n ; ++i ) cin >> arr[i];
+
+   vector < pair < ll , ll > > v;
+
+   v.push_back ( { 1 , arr[0] } );
+   v.push_back ( { 2 , arr[1] } );
+
+   ll mx = 1;
+
+   for ( ll i = 2 ; i < n ; ++i ){
+
+       while ( v.size() > 1 and slope ( v ,i+1 , arr[i] ) >= 0 ){
+        v.pop_back();
+       }
+
+       mx = max ( mx , (i+1) - v.back().first );
+
+       v.push_back ( { i+1 , arr[i] } );
+   }
+
+   /// for ( auto it : v ) cout << it.first << " " << it.second << "\n";
+
+   cout << mx << "\n";
+
+   return;
+}
+
+int main()
+{
+    MUHAMMAD
+
+   /// AE;
+    /*
+    #ifdef OJ
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    */
+
+    int t = 1 , tc = 0;
+
+    cin >> t;
+
+    while ( t-- ){
+       solve( ++tc );
+    }
+}
+
+
+/*
+
+
+Explanation:
+
+ Time :
+
+
+----------------------------------------------------------------------------------------------------------------
+
+
+
+  Alhamdulillah
+*/
